618e64da69d513e9bd6a6fc65dfd5f53b6d30743
krulvis <joepkleint@gmail.com>
2018-12-21 20:07:20 +0100

    Soemthing happen

--- database/seeds/CampaignItemTableSeeder.php
+++ database/seeds/CampaignItemTableSeeder.php
@@ -4,21 +4,19 @@ use App\Item;
 use App\User;
 use Illuminate\Database\Seeder;
 
-class CampaignItemTableSeeder extends Seeder
-{
+class CampaignItemTableSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-
+    public function run() {
         $user = User::where('name', '=', 'Joep KT')->first();
         DB::table('campaign_items')->insert([
             'item_id' => Item::first()->id,
             'campaign_id' => $user->campaigns()->first()->id,
-            'carried_by' => $user->id
+            'carried_by' => $user->id,
+            'owned_by' => $user->id
         ]);
         factory(App\CampaignItem::class, 100)->create();
     }
